270e7afbfcfaef1db8fc58c6ecabfe3eee97cc05
dkzhangchao <dkzhangchao@gmail.com>
2018-01-21 18:04:54 +0800

    A file was deleted

--- seiscode/3D:2D/src/conv/ThiesDL1/version.h
+++
@@ -1,7 +0,0 @@
-/*! \file version.h
- * \brief DL1logger software version indicator 
- */
-#ifndef DL1_LOGGER_SOFTWARE_VERSION
-#define DL1_LOGGER_SOFTWARE_VERSION "DL1logger software version V2014-03-31"
-#endif // DL1_LOGGER_SOFTWARE_VERSION (includeguard)
-/* ----- END OF version.h ----- */
--- seiscode/3D:2D/src/ts/lisousi/examples/lzline.pck
+++
@@ -1,4 +0,0 @@
-REFRACT   V4.9   REFRACTion seismics - data interpretation                                          
-    2
-   0.1700000   0.1000000
-   0.2000000   0.1010000
